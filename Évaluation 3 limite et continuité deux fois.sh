ca22eee433504fcbedfb23c437ea257e6a83aa8c
Vincent-Xavier JUMEL <maths@jumel.net>
2017-06-13 18:19:03 +0200

    Renommage
    Signed-off-by: Vincent-Xavier JUMEL <maths@jumel.net>

---
+++ Évaluation 3 limite et continuité deux fois.sh
@@ -0,0 +1,7 @@
+#! sh
+pdfnup -o "Évaluation 3 limite et continuité deux fois.pdf" \
+  --nup 1x2 \
+  --frame true \
+  --no-landscape \
+  "Évaluation 3A continuité.pdf" \
+  "Évaluation 3B continuité.pdf"
